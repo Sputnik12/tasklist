0d6351d6bf07d544f379e56e56b675f943351fa3
Aida <sputnik12@inbox.ru>
2019-06-24 20:25:29 +0300

    Init project

---
+++ tasklist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tasklist
+//
+//  Created by Aida Priimak on 24/06/2019.
+//  Copyright © 2019 Aida Priimak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
